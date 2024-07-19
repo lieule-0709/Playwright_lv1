b9c2502cf1c54968d055937ea47224375065aa83
Lieu Le <lieu.le@logigear.com>
2024-07-19 15:38:01 +0700

    Update TC027

--- pages/choose-panels-page.ts
+++ pages/choose-panels-page.ts
@@ -3,16 +3,21 @@ import presetPanels from 'fixtures/data/presetPanels.json'
 
 export default class ChoosePanelsPage {
     private readonly panelsLocator: Locator = this.page.locator(".pitem > .ptit");
+    private readonly createNewPanelBtn: Locator = this.page.locator("//div[@class='cpbutton']/span[contains(text(), 'Create new panel')]");
   
 
     constructor(private readonly page: Page) {}
+
+    async clickAddPanel(): Promise<void>{
+      await test.step("Click on 'Create new panel' button", async()=>{
+        await this.createNewPanelBtn.click();
+      })
+    }
   
 
     async presetPanelsDisplay(): Promise<void> {
         await test.step("Verify all pre-set panels are populated and sorted correctly", async () => {
           const panels = await this.panelsLocator.allTextContents();
-          await console.log(panels);
-          await console.log(presetPanels);
           await expect(panels).toEqual(presetPanels);
         });
       }
--- tests/tadashboard/panel/DA_PANEL_TC027.spec.ts
+++ tests/tadashboard/panel/DA_PANEL_TC027.spec.ts
@@ -1,13 +1,48 @@
 import { test } from "fixtures/common-fixture";
-import presetPanels from "fixtures/data/presetPanels.json";
+import DashboardMainPage from "pages/dashboard-main-page";
+import PanelsPage from "pages/panels-page";
+import { DateTimeHelper } from "support/helpers/date-time-helper";
+
+let dashboardMPage: DashboardMainPage;
+let panelsPageIns: PanelsPage;
+const pageName = DateTimeHelper.getToday();
+const panelName = "Zpanel" + DateTimeHelper.getToday();
 
 test("Verify that when 'Choose panels' form is expanded all pre-set panels are populated and sorted correctly", async ({
   dashboardMainPage,
-  newPageForm,
-  choosePanelsPage
+  choosePanelsPage,
+  newPanelForm,
+  panelsPage,
 }) => {
+  dashboardMPage = dashboardMainPage;
+  panelsPageIns = panelsPage;
 
+
+  // Navigate to Dashboard login page
+  // Login with valid account
+  // Go to Global Setting -> Add page
+  // await dashboardMainPage.selectSetting("Add Page");
+
+  // Enter page name to Page Name field.
+  // Click OK button
+  // await newPageForm.create({ pageName: pageName, public: true });
+
+  // Go to Global Setting -> Create Panel
+  await dashboardMainPage.selectSetting("Create Panel");
+  
+  // Enter Panel name into Display Name textbox
+  // Select any value in Series* dropdown list
+  // Click Ok button
+  // Click Ok button in Panel Configuration popup
+  await newPanelForm.create({displayName: panelName, series: "location"});
+
+  // Click on Choose Panel menu icon next to Global Setting icon
   await dashboardMainPage.expandChoosePanels();
   await choosePanelsPage.presetPanelsDisplay();
 });
 
+test.afterEach("Delete page", async () => {
+  await dashboardMPage.selectMenu(pageName);
+  await dashboardMPage.deletePageAndVerifyDialogMessage();
+  await panelsPageIns.deleteAllPanels();
+});
